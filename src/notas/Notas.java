6602fea4194a23f86bc31614f8e9fff725dcd81d
heleven-pc <heleven-pc@192.168.22.11>
2022-02-20 16:23:59 +0100

    2. Utiliza el analizador estático de código PMD en Eclipse

--- src/notas/Notas.java
+++ src/notas/Notas.java
@@ -11,16 +11,16 @@ public class Notas {
 		System.out.print("Introduzca una nota: ");
 		n = reader.nextInt();
 
-		if (n >= 0 && n < 5)
-			c = "Suspenso";
-		else if (n >= 5 && n < 7)
-			c = "Aprobado";
-		else if (n >= 7 && n < 9)
-			c = "Notable";
-		else if (n >= 9 && n <= 10)
-			c = "Sobresaliente";
-		else
-			c = "La nota introducida no es correcta";
+		if (n >= 0 && n < 5) {
+			c = "Suspenso";}
+		else if (n >= 5 && n < 7) {
+			c = "Aprobado";}
+		else if (n >= 7 && n < 9) {
+			c = "Notable";}
+		else if (n >= 9 && n <= 10) {
+			c = "Sobresaliente";}
+		else {
+			c = "La nota introducida no es correcta";}
 
 		System.out.println(c);
 		reader.close();
