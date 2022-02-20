241531b49ab79a02583f1e96453a5c83a5d5ee1e
heleven-pc <heleven-pc@192.168.22.11>
2022-02-20 18:04:25 +0100

    5. Utiliza refactorización para cambiar la cabecera del método

--- src/notas/Notas.java
+++ src/notas/Notas.java
@@ -11,7 +11,7 @@ public class Notas {
 		System.out.print("Introduzca una nota: ");
 		nota = reader.nextInt();
 
-		calificacion = cali(nota);
+		calificacion = califica(nota);
 
 		System.out.println(calificacion);
 		reader.close();
@@ -20,16 +20,25 @@ public class Notas {
 	/**
 	 * @param nota
 	 * @return calificacion
+	 * @deprecated Use {@link #califica(int)} instead
 	 */
 	public static String cali(int nota) {
+		return califica(nota);
+	}
+
+	/**
+	 * @param num_nota
+	 * @return calificacion
+	 */
+	public static String califica(int num_nota) {
 		String calificacion;
-		if (nota >= 0 && nota < 5) {
+		if (num_nota >= 0 && num_nota < 5) {
 			calificacion = "Suspenso";}
-		else if (nota >= 5 && nota < 7) {
+		else if (num_nota >= 5 && num_nota < 7) {
 			calificacion = "Aprobado";}
-		else if (nota >= 7 && nota < 9) {
+		else if (num_nota >= 7 && num_nota < 9) {
 			calificacion = "Notable";}
-		else if (nota >= 9 && nota <= 10) {
+		else if (num_nota >= 9 && num_nota <= 10) {
 			calificacion = "Sobresaliente";}
 		else { 
 			calificacion = "La nota introducida no es correcta";}
